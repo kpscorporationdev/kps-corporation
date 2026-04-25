3b97ea72c199499ebfd36beeabd2a6a8adad4741
kpscorporationdev <kpscorporation0@gmail.com>
2026-04-25 18:43:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const client = new Client({
 require('./rayuna')(client);
 require('./bienvenue-streetnova')(client);
 require('./reglement-streetnova')(client);
+require('./topia-fr')(client);
 
 client.once('ready', () => {
   console.log(`✅ Bot connecté en tant que ${client.user.tag}`);
